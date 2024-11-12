07fe33e76ffbb9f4d88df5df5fe590895fb61124
Miguel <mendezlie@hotmail.com>
2024-11-12 20:11:31 +0100

    fin de proyecto. saludo realizado

---
+++ src/main/java/com/curso/java/miguel/ejecutable/Ejecutable.java
@@ -0,0 +1,11 @@
+package com.curso.java.miguel.ejecutable;
+
+import com.curso.java.miguel.service.Saludo;
+
+public class Ejecutable {
+
+	public static void main(String[] args) {
+		Saludo.saludar();
+	}
+
+}
